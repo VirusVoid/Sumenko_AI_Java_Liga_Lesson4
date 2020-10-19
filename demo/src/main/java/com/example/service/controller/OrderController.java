d36edaf2a23efddc8b55d828139c29404412ac02
ALeksa <aleksa.sumenko@yandex.ru>
2020-10-19 13:45:15 +0300

    master решение задачи 4 урока

--- demo/src/main/java/com/example/service/controller/OrderController.java
+++ demo/src/main/java/com/example/service/controller/OrderController.java
@@ -4,9 +4,13 @@ import com.example.service.model.Orders;
 import com.example.service.dao.OrderDAO;
 import com.example.service.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 /**
  * Добавление заказа
@@ -19,5 +23,14 @@ public class OrderController {
     @PostMapping(value = "/api/v1/order")
     public ResponseEntity<String> createOrder(@RequestBody Orders order) {
         return orderService.createOrderForCustomer(order);
+    }
+}
+
+@ControllerAdvice
+class RestExceptionHandler extends ResponseEntityExceptionHandler {
+    @Override
+    protected ResponseEntity handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+                                                          HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return new ResponseEntity("Некорректный JSON запрос (статус: "+status+") \n"+ ex.toString(), HttpStatus.BAD_REQUEST);
     }
 }
